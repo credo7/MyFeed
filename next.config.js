94caa2ab2bf9e373012526cf3a6ded44acbde60d
credo7 <ah.vit@mail.ru>
2022-05-06 23:40:59 +0300

    add Basepath + assetPrefix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const nextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+    basepath: "/MyFeed",
+    assetPrefix: "/MyFeed",
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
