9d3e92b94be3587bda6dafab7c8cc993b1ffaeb0
credo7 <73043804+credo7@users.noreply.github.com>
2022-05-06 20:31:13 +0000

    Deploying to gh-pages from @ credo7/MyFeed@cdb1f0a3c260b9031fa530714c95d603fb961457 🚀

---
+++ _next/static/chunks/d64684d8-d48528b23a0190ff.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[556],{6084:function(t,n,r){r.d(n,{dVn:function(){return o}});var e=r(8357);function o(t){return(0,e.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M12 19l9 2-9-18-9 18 9-2zm0 0v-8"}}]})(t)}}}]);
