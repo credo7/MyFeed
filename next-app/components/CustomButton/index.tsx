92bbf44128d3b90ff0fda585482ce6e52c957d2d
credo7 <ah.vit@mail.ru>
2022-05-06 23:10:54 +0300

    File reconstruction

--- next-app/components/CustomButton/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function CustomButton(props: { children: ReactNode }) {
-  return (
-    <button className="py-[8px] px-[40px] bg-white border-[1px] rounded-[3px] text-sm">
-      {props.children}
-    </button>
-  );
-}
